f87b0872997ada9554367bd6086632aa774cd552
mleidel <michaelleidel321@gmail.com>
2024-08-26 16:39:16 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # wordScriber
-HTML local document editor
+
+__HTML local document editor__  
+Linux version
+
+This project demonstrates how to create apps that 
+combine a python module (tkinter) and an HTML GUI 
+in an offline desktop situation. In this case the 
+focus is on using HTML to edit, create and format,
+HTML documents. Here pywebview provides communication between
+Python/tkinter and HTML/Javascript.
+
+There are certain limitations depending on the web engin
+and API employed. Web engines used outside of an Internet 
+browser may be missing features found in the browser versions.
+In this example pywebview (_WebKitGTK_) is used in a linux environment.
+
+In the Linux case (WebKitGTK) spell checking is absent. 
+In the Windows version of this project pywebview
+uses _WebView2_ (part of Edge). In Windows testing
+spell checking does work in a limited fashion. 
+
+Also this Linux version has some extra code 
+to force the tkinter dialogs to open on the correct screen
+of a dual monitor setup. If you are using a single monitor, 
+then remove the indicated code from wsr.py.
+
+As you may have realized, I'm not using the webview module
+to bring Internet pages into a desktop environment. This project
+could be easily modified to do that, and there are easier ways.
+The purpose of this project is to demonstrate how the pywebview module 
+provides communication between Python/tkinter and HTML/Javascript.
+
+
---
+++ wsr.py
@@ -0,0 +1,139 @@
+import webview
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import subprocess
+import webbrowser
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+current_file = "docs/untitled.html"  # tracks current file in use
+
+def window_coord():
+    px = window.x
+    py = window.y
+    geo = f"20x20+{px}+{py}"
+    return geo
+
+def on_closing():
+    return True
+    # Issues
+    # If using single monitor delete 'return True' above and uncomment below
+    # if messagebox.askokcancel("Save File?", "Really want to close this App?"):
+    #     return True
+    # else:
+    #     return False
+
+'''
+API class to handle various functions
+that typically could not be handled by a browser sponsored HTML GUI
+because of restricted access to the user's system.
+'''
+class Api:
+
+    def onClose(self):
+        ''' immediate close no ask '''
+        window.events.closing -= on_closing
+        window.destroy()
+
+    def getFileName(self):
+        global current_file
+        return current_file
+
+    def open_file(self):
+        ''' called from javascript '''
+        root = tk.Tk()  # remove if single monitor
+        root.geometry(window_coord())  # remove if single monitor
+        global current_file
+        file_path = filedialog.askopenfilename(initialdir="./docs",
+                                               title = "Open file",
+                                               filetypes = (("HTML", "*.html"),
+                                                            ("all files", "*.*")))
+        root.destroy()  # remove if single monitor
+        if file_path:
+            current_file = file_path
+            with open(current_file, 'r') as file:
+                return file.read()
+        return ''
+
+    def save_file(self, content):
+        ''' called from javascript '''
+        root = tk.Tk()  # remove if single monitor
+        root.geometry(window_coord())  # remove if single monitor
+        global current_file
+        file_path = filedialog.asksaveasfilename(initialdir="./docs",
+                                                 defaultextension=".html",
+                                                 initialfile="",
+                                                 filetypes = (("HTML", "*.html"),
+                                                            ("all files", "*.*")))
+        root.destroy()  # remove if single monitor
+        if file_path:
+            current_file = file_path
+            with open(file_path, 'w') as file:
+                file.write(content)
+            return current_file
+        return ''
+
+    def quick_save_file(self, content):
+        global current_file
+        file_path = current_file
+        with open(file_path, 'w') as file:
+            file.write(content)
+        current_file = file_path
+        return current_file
+
+    def open_options(self):
+        ''' called from javascript '''
+        with open("options.dat", 'r') as file:
+            lst = [line for line in file if not line.lstrip().startswith('=')]
+            lst = [i.strip() for i in lst]  # strip end of lines
+            return ','.join(lst)  # make a csv string
+        return ''
+
+    def open_editor(self):
+        subprocess.Popen([tx, "./options.dat"])
+
+    def open_filemgr(self):
+        subprocess.Popen([fm, "./"])
+
+    def open_browser(self):
+        # launch current file in browser
+        webbrowser.open(current_file)
+
+    def open_spellcheck(self, content):
+        # process sting of words and return results
+        print(content)
+        wlist = content.split()         # string to list
+        words = spell.unknown(wlist)    # spell check the list
+        strwords = "<br>"
+        for word in words:              # concat spell output into one string
+            strwords += word + "<br>"
+            # Get the one `most likely` answer
+            #   spell.correction(word)
+            # Get a list of `likely` options
+            strwords += str(spell.candidates(word))
+            strwords += "<hr>"
+        print (strwords)
+        return strwords
+
+# Create a webview window
+if __name__ == '__main__':
+
+    api = Api()
+
+    opts = api.open_options().split(',')
+    w = int(opts[7]) # width
+    h = int(opts[8]) # height
+    tx = opts[9]  # text editor
+    fm = opts[10] # file manager
+
+    window = webview.create_window('Word Scriber',
+                     url='index.html',
+                     width=w,
+                     height=h,
+                     js_api=api)
+
+    window.events.closing += on_closing
+
+    webview.start()
